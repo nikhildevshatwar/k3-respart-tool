55ca507236e6db931f47312da48b925fbf469c63
PrinceRathi859 <princerathi859@gmail.com>
2020-05-18 17:02:09 +0530

    Commit

--- modules/hostConfig.syscfg.js
+++ modules/hostConfig.syscfg.js
@@ -264,6 +264,30 @@ exports = {
                         default : [0,1,2,3],
                 },
               ],
+              pinmuxRequirements: (inst) => {
+                      return [
+                                {
+                                        name: "interrupt_router_for_c6x_0",
+                                        displayName: "Interrupt router for C6x_0",
+                                        interfaceName: "GPIO"
+                                },
+                                {
+                                        name: "Interrupt_router_for_c6x_1",
+                                        displayName: "Interrupt router for C6x_1",
+                                        interfaceName: "GPIO"
+                                },
+                                {
+                                        name: "compare_event_ir",
+                                        displayName: "compare event IR",
+                                        interfaceName: "GPIO"
+                                },
+                                {
+                                        name: "main_2_mcu_level_irq_ir",
+                                        displayName: "Main 2 MCU level IRQ IR",
+                                        interfaceName: "GPIO"
+                                }
+                      ];
+              }
 };
 
 
