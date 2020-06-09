6db32083391e805bffc61fb6040dbc5c2db206db
Nikhil Devshatwar <nikhil.nd@ti.com>
2020-06-09 16:24:47 +0530

    data: SOC: Add support for AM64x
    Modify scripts to generate the data for am664x
    Create blank ResDependencies.json file
    Add entry in SOC.json for am64x and in product.json
    
    HACK: Add AM437x as a placeholder for device selection
    till SysConfig officially supports this device name.
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- scripts/update-json-files.sh
+++ scripts/update-json-files.sh
@@ -2,6 +2,13 @@ topdir=`git rev-parse --show-toplevel`
 sysfw_repo=`cd $topdir/../system-firmware; pwd`
 csl_repo=`cd $topdir/../csl; pwd`
 
+prettify_json() {
+file=$1
+	jq --tab --sort-keys . $file > pretty.json
+	mv pretty.json $file
+	echo "  Converted to pretty format"
+}
+
 gen_files() {
 soc=$1
 	case $soc in
@@ -13,25 +20,32 @@ soc=$1
 			sysfw_soc=am6x
 			csl_soc=
 			;;
+		am64x)
+			sysfw_soc=am64x
+			csl_soc=am64x
+			;;
 	esac
 
 	# Parse SYSFW documentation Resource assignments
 	node $topdir/scripts/parse_resource_rst.js --soc $soc \
 	  --doc $sysfw_repo/docs/public/5_soc_doc/$sysfw_soc/resasg_types.rst \
 	  --dep $topdir/data/$soc/ResDependencies.json
-	echo "Generated data/$soc/ResDependencies.json"
-	cat $topdir/data/$soc/Resources.json | json_pp -json_opt pretty > pretty.json
-	mv pretty.json $topdir/data/$soc/Resources.json
-	echo "  Converted to pretty format"
+	echo "Generated data/$soc/Resources.json"
+	prettify_json $topdir/data/$soc/Resources.json 
+
+	# Parse SYSFW documentation Host descriptions
+	node $topdir/scripts/parse_hostname_rst.js --soc $soc \
+	  --doc $sysfw_repo/docs/public/5_soc_doc/$sysfw_soc/hosts.rst \
+	  --firewall $sysfw_repo/docs/public/5_soc_doc/$sysfw_soc/firewalls.rst
+	echo "Generated data/$soc/Hosts.json"
+	prettify_json $topdir/data/$soc/Hosts.json 
 
 	# Parse CSL SoC QoS data
-	if [ "$csl_soc" != "" ]; then
+	if [ -f $csl_repo/soc/$csl_soc/src/csl_soc_qos.h ]; then
 		node $topdir/scripts/parse_csl_soc_qos_h.js --soc $soc \
 		   --doc $csl_repo/soc/$csl_soc/src/csl_soc_qos.h
 		echo "Generated data/$soc/Qos.json"
-		cat $topdir/data/$soc/Qos.json | json_pp -json_opt pretty > pretty.json
-		mv pretty.json $topdir/data/$soc/Qos.json
-	echo "  Converted to pretty format"
+		prettify_json $topdir/data/$soc/Qos.json 
 	fi
 }
 
@@ -41,6 +55,7 @@ if [ "$1" = "" ]; then
 elif [ "$1" = "all" ]; then
 	gen_files j721e
 	gen_files am65x
+	gen_files am64x
 else
 	gen_files $1
 fi
