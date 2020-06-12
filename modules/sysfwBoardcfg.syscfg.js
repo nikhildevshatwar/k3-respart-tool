193093aa76e914969a1245836ebca8bb5f15f314
PrinceRathi859 <princerathi859@gmail.com>
2020-06-12 12:56:58 +0530

    Add board-config module
    Add module and template for board-Config
    Add configurables for ram and debug
    Add baseline for j721e

---
+++ modules/sysfwBoardcfg.syscfg.js
@@ -0,0 +1,38 @@
+const deviceSelected = system.deviceData.device;
+const devData = _.keyBy(system.getScript("/data/SOC.json"),(r) => r.soc);
+
+function createRAMSizeOptions(){
+        var opt = [];
+
+        var gran = devData[deviceSelected].granularity;
+        var curr = 0;
+        var size = devData[deviceSelected].sRAMSize;
+
+        while(curr <= size){
+                opt.push({
+                        name: curr,
+                        displayName: curr.toString() + " " + "MB"
+                })
+
+                curr += gran;
+        }
+
+        return opt;
+}
+
+exports = {
+        displayName: "SYSFW Board Config",
+        config: [
+                {
+                        name: "ramSize",
+                        displayName: "RAM Size",
+                        options: createRAMSizeOptions(),
+                        default: 0
+                },
+                {
+                        name: "debugEnable",
+                        displayName: "Debug Enable",
+                        default: false
+                }
+        ]
+}
