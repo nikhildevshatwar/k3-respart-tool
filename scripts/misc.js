0847eb93bf2480b887702bcb353507851c94e30f
Nikhil Devshatwar <nikhil.nd@ti.com>
2020-07-02 13:52:25 +0530

    scripts: Generalize the copy attribs function
    Generalize the function such that copying attributes from
    different JSON files is simplified.
    This can be used for DeviceNames.json or HostNames.json
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,22 +1,44 @@
 var fs = require('fs');
 
-function copy-devnames() {
-	var input = JSON.parse(fs.readFileSync("./data/am65x/DeviceName.json").toString());
-	var output = JSON.parse(fs.readFileSync("./data/am64x/DeviceName.json").toString());
+function copy_attributes(inp, out, match, attrib) {
+
+	console.log(`Copying attributes "${attrib}" from ${inp} to ${out} when "${match}" matches`);
+
+	var input = JSON.parse(fs.readFileSync(inp).toString());
+	var output = JSON.parse(fs.readFileSync(out).toString());
+
+	if (input == undefined || output == undefined)
+		return;
 
 	processed = []
 	output.forEach(out => {
 		input.forEach(inp => {
-			if (JSON.stringify(inp.protected_inst) == JSON.stringify(out.protected_inst)) {
-				out.name = inp.name
+			if (JSON.stringify(inp[match]) == JSON.stringify(out[match])) {
+				out[attrib] = inp[attrib]
 			}
 		})
-		console.log(out.name)
 		processed.push(out)
 	})
 	output = processed
 
-	fs.writeFile("./data/am64x/Processed.json", JSON.stringify(output), (err) => {
+	fs.writeFile(out, JSON.stringify(output), (err) => {
 		if (err) throw err;
 	})
+
+	console.log(`Prettify as below\n jq --tab --sort-keys . ${out} >pretty && mv pretty ${out}`)
 }
+
+function copyAll(soclist, soc, match, attrib) {
+
+	soclist.forEach(s => {
+		if (s == soc)
+			return;
+
+		var inp = `./data/${s}/HostNames.json`
+		var out = `./data/${soc}/HostNames.json`
+		copy_attributes(inp, out, match, attrib)
+	});
+}
+
+//copyAll(["j721e"], "j7200", "protected_inst", "name");
+//copyAll(["j721e"], "j7200", "hostName", "displayName");
